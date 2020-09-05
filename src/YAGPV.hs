2be8d812050826fd6a264946c78a0785196714de
ocramz <zocca.marco@gmail.com>
2020-09-05 15:50:11 +0200

    saving

--- src/YAGPV.hs
+++ src/YAGPV.hs
@@ -2,14 +2,15 @@
 {-# options_ghc -Wno-unused-imports #-}
 module YAGPV where
 
+import Data.String (IsString(..))
 -- containers
-import Data.Tree (Tree(..), drawTree, Forest)
+import Data.Tree (Tree(..), drawTree, Forest, foldTree, unfoldTree)
 -- ghc-prof
 import qualified GHC.Prof as P (Profile(..), decode, CostCentre(..), costCentres )
 -- scientific
 import Data.Scientific (Scientific)
 -- text
-import qualified Data.Text as T (Text, splitOn)
+import qualified Data.Text as T (Text, splitOn, pack, unpack)
 import qualified Data.Text.Lazy as TL (Text, fromStrict, toStrict)
 import qualified Data.Text.Lazy.IO as TL (readFile)
 
@@ -19,7 +20,18 @@ data CCSummary = CCS {
   , ccsModule :: T.Text
   , ccsEntries :: !Integer
   , ccsInhTime :: Scientific
-                     } deriving (Eq, Show)
+                     } deriving (Eq)
+instance Show CCSummary where
+  show (CCS n m e t) = unwords [T.unpack n, T.unpack m, show e, show t]
+
+-- prune q t = filter q <$> t
+
+-- prune q = foldTree (\l xs -> if q l then xs else Node l xs)
+
+-- prune q = foldTree $ \ l xs ->
+--   case q l of
+--     True -> Node l xs
+--     False -> Node l xs
 
 ccSummary :: P.CostCentre -> CCSummary
 ccSummary s = CCS name' mdl nents iht
